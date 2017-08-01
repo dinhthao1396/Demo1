04c8edcf6f275fdf88cf46f2238ff9d90605454e
Tran Dinh Thao <dinhthao1396@gmail.com>
2017-08-01 17:37:45 +0700

    first

---
+++ Demo1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Demo1
+//
+//  Created by Tran Dinh Thao on 6/7/17.
+//  Copyright © 2017 Tran Dinh Thao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Demo1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by Tran Dinh Thao on 6/7/17.
+//  Copyright © 2017 Tran Dinh Thao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
